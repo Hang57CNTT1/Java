8559e7cf14448569a6b3979432c47c8c26dc6ad9
Nguyễn Thị Hằng <37855107+Hang57CNTT1@users.noreply.github.com>
2018-04-11 10:06:43 +0700

    Bài TH2 Swing&ArrayList

---
+++ BaiTH2_SW/Program.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baith2_sw;
+
+/**
+ *
+ * @author Hang
+ */
+public class Program extends javax.swing.JFrame{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Login().setVisible(true);
+            }
+        });
+    }
+    private javax.swing.JPasswordField Pass;
+    private javax.swing.JButton btnCancel;
+    private javax.swing.JButton btnDangNhap;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JTextField txtName;
+}
---
+++ BaiTH2_SW/StudentInfo.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baith2_sw;
+
+/**
+ *
+ * @author Hang
+ */
+public class StudentInfo {
+        private String ten;
+        private String ns;
+         private String gt;
+
+    /**
+     * @return the ten
+     */
+    public String getTen() {
+        return ten;
+    }
+
+    /**
+     * @param ten the ten to set
+     */
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    /**
+     * @return the ns
+     */
+    public String getNs() {
+        return ns;
+    }
+
+    /**
+     * @param ns the ns to set
+     */
+    public void setNs(String ns) {
+        this.ns = ns;
+    }
+
+    /**
+     * @return the gt
+     */
+    public String getGt() {
+        return gt;
+    }
+
+    /**
+     * @param gt the gt to set
+     */
+    public void setGt(String gt) {
+        this.gt = gt;
+    }
+
+    void setGt() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+        
+        
+}

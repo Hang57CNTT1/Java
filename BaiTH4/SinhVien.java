f6ac26b25ef887a67195bfc7f4468534a6ab2770
Nguyễn Thị Hằng <37855107+Hang57CNTT1@users.noreply.github.com>
2018-04-23 10:39:33 +0700

    BaiTH4

---
+++ BaiTH4/SinhVien.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baith4_model;
+
+/**
+ *
+ * @author Hang
+ */
+class SinhVien {
+    private String mssv;
+    private String hoten;
+    private String ns;
+    private String lop;
+    
+    /**
+     * @return the mssv
+     */
+    public String getMssv() {
+        return mssv;
+    }
+
+    /**
+     * @param mssv the mssv to set
+     */
+    public void setMssv(String mssv) {
+        this.mssv = mssv;
+    }
+
+    /**
+     * @return the hoten
+     */
+    public String getHoten() {
+        return hoten;
+    }
+
+    /**
+     * @param hoten the hoten to set
+     */
+    public void setHoten(String hoten) {
+        this.hoten = hoten;
+    }
+
+    /**
+     * @return the ns
+     */
+    public String getNs() {
+        return ns;
+    }
+
+    /**
+     * @param ns the ns to set
+     */
+    public void setNs(String ns) {
+        this.ns = ns;
+    }
+
+    /**
+     * @return the lop
+     */
+    public String getLop() {
+        return lop;
+    }
+
+    /**
+     * @param lop the lop to set
+     */
+    public void setLop(String lop) {
+        this.lop = lop;
+    }
+    
+}
